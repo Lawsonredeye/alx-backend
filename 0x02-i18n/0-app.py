ae4ffbc4aa1bda17a2970b69a154fb4ae8f8b004
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-07-30 21:35:45 +0100

    (fix): added documentation

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
+"""Module which handles the web app logic and routes as well as
+the internationalization and localization"""
 
 from flask import Flask
 from flask import render_template
 
-
 app: Flask = Flask(__name__)
+
 
 @app.route("/")
 def index() -> str:
+    """Homepage route for Holberton webpage"""
     return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
