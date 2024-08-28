268a541e5b0516ddbc1bd1ef9a2fd1c153183015
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-08-28 13:05:25 +0100

    Kue for handling push notifications by using the kue library for handling notifications

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,26 @@
+const kue = require('kue');
+
+const jobObject = {
+  phoneNumber: "",
+  message: ""
+};
+
+// Creates Queue object
+const push_nofication_code = kue.createQueue();
+
+const jobs = push_nofication_code.create('notification',jobObject).save((err) => {
+  if (!err) {
+    console.log(`Notification job created: ${jobs.id}`);
+  }
+})
+
+// Check status of the Queue progress while being active
+jobs.on('progress', (progress, data) => {
+  console.log(`Notification job created: ${jobs.id}`);
+})
+.on('complete', () => {
+  console.log('Notification job completed');
+})
+.on('failed', (result) => {
+  console.log('Notification job failed');
+})
